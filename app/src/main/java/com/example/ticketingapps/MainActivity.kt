4b6870e2e4f46072379fe1110abd55206dee5e6d
AdamNizam <AdmNizamwekaweka37@gmail.com>
2024-09-10 20:39:12 +0700

    Add Object Canvas

--- app/src/main/java/com/example/ticketingapps/MainActivity.kt
+++ app/src/main/java/com/example/ticketingapps/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.ticketingapps
 
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val seatView =  findViewById<SeatsView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
+
+        button.setOnClickListener {
+            seatView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}.", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silakan pilih kursi terlebih dahulu.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+//        //  Jika Anda ingin memperbesar tampilan dan menghilangkan toolbar
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        supportActionBar?.hide()
+
     }
 }
